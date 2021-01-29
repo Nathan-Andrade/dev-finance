cfc472404e51b52884c2eb5ff51ad6e2cc0c6f68
Nathan Andrade <nathandulino2003@gmail.com>
2021-01-28 23:01:37 -0200

    add js

--- index.js
+++ index.js
@@ -8,3 +8,39 @@ const Modal = {
     document.querySelector('.modal-overlay').classList.remove('active')
   }
 }
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Água',
+    amount: -20000,
+    date: '21/01/2021'
+  },
+  {
+    id: 2,
+    description: 'Projeto de site',
+    amount: 20000,
+    date: '21/01/2021'
+  },
+  {
+    id: 3,
+    description: 'Luz',
+    amount: -50000,
+    date: '21/01/2021'
+  },
+];
+
+const Transaction = {
+  //somar as entradas
+  incomes(){
+
+  },
+  //somar as saídas
+  expenses(){
+
+  },
+  //soma das entradas - saídas
+  total(){
+
+  }
+}
