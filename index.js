02f680f5f52a9fa5b6c18ea434feb37c86284669
Nathan Andrade <nathandulino2003@gmail.com>
2021-01-30 00:23:27 -0200

    add js

--- index.js
+++ index.js
@@ -46,17 +46,24 @@ const Transaction = {
 }
 
 const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  
   addTransaction(transaction, index){
-    console.log('Working...')
     const tr = document.createElement('tr')
-    tr.innerHTML = DOM.innerHTMLTransaction()
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
   },
 
-  innerHTMLTransaction() {
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction)
+
     const html = `
-        <td class="description">Luz</td>
-        <td class="expense">- R$ 500,00</td>
-        <td class="date">23/01/2021</td>
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
         <td>
           <img src="./assets/minus.svg" alt="Remover transação">
         </td>
@@ -66,4 +73,12 @@ const DOM = {
   }
 }
 
-DOM.addTransaction()
+const Utils = {
+  formatCurrency(value){
+    const signal = Number(value) < 0 ? "-" : ""
+  }
+}
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction)
+})
