866b3c71a651e78c29ca35661b997b91ce921d4d
Nathan Andrade <nathandulino2003@gmail.com>
2021-01-30 22:58:57 -0200

    add js

--- index.js
+++ index.js
@@ -9,39 +9,62 @@ const Modal = {
   }
 }
 
-const transactions = [
-  {
-    id: 1,
-    description: 'Água',
-    amount: -20000,
-    date: '21/01/2021'
-  },
-  {
-    id: 2,
-    description: 'Projeto de site',
-    amount: 20000,
-    date: '21/01/2021'
-  },
-  {
-    id: 3,
-    description: 'Luz',
-    amount: -50000,
-    date: '21/01/2021'
-  },
-];
-
 const Transaction = {
+  all: [
+    {
+      description: 'Água',
+      amount: -20000,
+      date: '21/01/2021'
+    },
+    {
+      description: 'Projeto de site',
+      amount: 600000,
+      date: '21/01/2021'
+    },
+    {
+      description: 'Luz',
+      amount: -50000,
+      date: '21/01/2021'
+    },
+  ],
+
+  add(transaction){
+    Transaction.all.push(transaction)
+
+    App.reload()
+  },
+
+  remove(index){
+    Transaction.all.splice(index, 1)
+
+    App.reload()
+  },
+
   //somar as entradas
   incomes(){
-
+    let income = 0;
+    //pegar todas as transacoes
+    Transaction.all.forEach(transaction => {
+      if (transaction.amount > 0){
+        income += transaction.amount;
+      }
+    })
+    return income;
   },
   //somar as saídas
   expenses(){
-
+    let expense = 0;
+    //pegar todas as transacoes
+    Transaction.all.forEach(transaction => {
+      if (transaction.amount < 0){
+        expense += transaction.amount;
+      }
+    })
+    return expense;
   },
   //soma das entradas - saídas
   total(){
-
+    return Transaction.incomes() + Transaction.expenses();
   }
 }
 
@@ -58,11 +81,11 @@ const DOM = {
   innerHTMLTransaction(transaction) {
     const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
-    const amount = Utils.formatCurrency(transaction)
+    const amount = Utils.formatCurrency(transaction.amount)
 
     const html = `
         <td class="description">${transaction.description}</td>
-        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
           <img src="./assets/minus.svg" alt="Remover transação">
@@ -70,15 +93,88 @@ const DOM = {
     `
 
     return html
+  },
+
+  updateBalance() {
+    document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes());
+    document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses());
+    document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total());
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = ""
   }
 }
 
 const Utils = {
   formatCurrency(value){
     const signal = Number(value) < 0 ? "-" : ""
+
+    value = String(value).replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+
+    return signal + value
   }
 }
 
-transactions.forEach(function (transaction) {
-  DOM.addTransaction(transaction)
-})
+const Form = {
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
+
+  getValues(){
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value
+    }
+  },
+
+  validateFields(){
+    const { description, amount, date } = Form.getValues()
+   
+    if(description.trim() === "" || amount.trim() === "" || date.trim() === ""){
+      throw new Error("Por favor, preencha todos os campos")
+    }
+  },
+  submit(event){
+    event.preventDefault()
+
+    try{
+      Form.validateFields()
+      //verificar se todas as informações foram preenchidas
+
+    } catch (error){
+      alert(error.message)
+    }
+
+   
+
+  }
+}
+
+const App = {
+  init(){
+
+    Transaction.all.forEach(function (transaction) {
+      DOM.addTransaction(transaction)
+    })
+    
+    DOM.updateBalance();
+    
+    
+  },
+
+  reload() {
+    DOM.clearTransactions()
+    App.init()
+  },
+}
+
+App.init();
